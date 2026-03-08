6f6f7982a7c54415e6ee20a8e348caa4e5c711fd
nurulislamrimon <nurulislamrimon@gmail.com>
2026-03-08 12:34:12 +0600

    GITIGNORE_UPDATED

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"JyYRBS09nHOpUlxZRHhuSKwElGoUEsBhZdUyFNP9NxE=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"x3eFxamxOHF3ZSf0BNri/PyyAjEZEbgd9puv9aFUwRc=\"\n}"
